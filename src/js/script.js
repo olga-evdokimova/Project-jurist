515f3cd07ed2a1dbdc6a2ecdec19467025084ac5
pavedom13 <pavedom13@gmail.com>
2021-02-02 21:05:05 +0300

    txemble, slider

--- src/js/script.js
+++ src/js/script.js
@@ -162,3 +162,30 @@ DynamicAdapt.prototype.arraySort = function (arr) {
 
 const da = new DynamicAdapt("max");
 da.init();
+
+// Инициализируем  Swiper
+new Swiper('.swiper-container', {
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 2,
+    spaceBetween: 30, 
+
+    // breakpoints: {
+    //     '@0.75': {
+    //         slidesPerView: 1,
+    //     },
+    //     '@1.00': {
+    //         slidesPerView: 2,
+    //     },
+    //     '@1.50': {
+    //         slidesPerView: 3,
+    //     }
+    // },
+});
